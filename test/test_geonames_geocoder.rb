69e0b5a5cae0cb2924217223fba4f50f82932097
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-11-18 11:12:54 +0300

    Added a test for geonames search

--- test/test_geonames_geocoder.rb
+++ test/test_geonames_geocoder.rb
@@ -24,4 +24,18 @@ class GeonamesGeocoderTest < BaseGeocoderTest #:nodoc: all
     assert_equal "San Francisco, California, 94102, US", res.full_address
     assert_equal "geonames", res.provider
   end
+
+  def test_geonames_search
+    response = MockSuccess.new
+    response.expects(:body).returns(GEONAMES_SEARCH_RESULT)
+    url = "http://ws.geonames.org/search?q=#{geonames_escape(@address)}&style=FULL&maxRows=10"
+    Geokit::Geocoders::GeonamesGeocoder.expects(:call_geocoder_service).with(url).returns(response)
+    res=Geokit::Geocoders::GeonamesGeocoder.search(@address)
+    assert_equal "CA", res.state
+    assert_equal "San Francisco", res.city 
+    assert_equal "37.7749295,-122.4194155", res.ll
+    assert res.is_us?
+    assert_equal "San Francisco, CA, US", res.full_address
+    assert_equal "geonames", res.provider
+  end
 end
